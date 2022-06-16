efd4e9fb18476c814cce4694fed697ffe50d5bad
Fabian Guevara <fabianguevara.es@unitropico.edu.co>
2022-06-15 20:32:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rutinap/SplashScreen/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.rutinap.SplashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.rutinap.MainActivity;
+import com.example.rutinap.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Thread timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        timer= new Thread(){
+            public  void run(){
+                try{
+                    synchronized (this){
+                        wait(2850);
+                    }
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent =new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
